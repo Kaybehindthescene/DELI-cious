8f2cd7a39c74640898b9985493758cf9215d47c1
CalebShufford <cshufford@my.yearupunited.org>
2025-11-13 23:04:46 -0500

    added a PhillyCheeseSteak class

--- src/main/java/com/pluralsight/models/PhillyCheeseSteak.java
+++ src/main/java/com/pluralsight/models/PhillyCheeseSteak.java
@@ -1,4 +1,18 @@
 package com.pluralsight.models;
 
-public class PhillyCheeseSteak {
+public class PhillyCheeseSteak extends Sandwich {
+
+    public PhillyCheeseSteak(){
+        super(Size.EIGHT,Bread.WHITE,true);
+        addTopping(
+                Topping.ROAST_BEEF,
+                Topping.AMERICAN,
+                Topping.PEPPERS,
+                Topping.MAYO
+        );
+    }
+    @Override
+    public String getLabel(){
+        return "Philly CheeseSteak (" + super.getLabel() + ")"
+    }
 }
