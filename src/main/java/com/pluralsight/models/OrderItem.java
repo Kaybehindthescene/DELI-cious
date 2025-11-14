a0974727f117b3f721b1bd0cd295b045acb2447c
CalebShufford <cshufford@my.yearupunited.org>
2025-11-13 22:32:23 -0500

    added comments to OrderItem interface

--- src/main/java/com/pluralsight/models/OrderItem.java
+++ src/main/java/com/pluralsight/models/OrderItem.java
@@ -1,5 +1,8 @@
 package com.pluralsight.models;
-
+// Represents an item that can appear on a customer's order.
+// This interface allows different kinds of items (Sandwiches, Drinks, Chips,
+// Desserts, etc.) to be handled the same way when calculating totals
+// or printing the receipt.
 public interface OrderItem {
     String getLabel();
     double getUnitPrice();
